07b1e7e7e1edf7dc5db57b029b9ce92cfffe72fa
Dixon <dixonwillow23@gmail.com>
2023-11-23 18:35:21 +0700

    feat: create transaction detail logic

--- src/model/TransactionDetail.java
+++ src/model/TransactionDetail.java
@@ -1,32 +1,101 @@
 package model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.Connect;
+
 public class TransactionDetail {
 	private Integer transactionId;
-	private Integer userId;
+	private Integer itemId;
 	private Integer quantity;
-	public TransactionDetail(Integer transactionId, Integer userId, Integer quantity) {
-		super();
+	
+	public TransactionDetail(Integer transactionId, Integer itemId, Integer quantity) {
 		this.transactionId = transactionId;
-		this.userId = userId;
+		this.itemId = itemId;
 		this.quantity = quantity;
 	}
-	public Integer getTransactionId() {
-		return transactionId;
+	
+	public void addTransactionDetail(Integer transactionId, Integer itemId, Integer quantity) {
+		
+		String query = String.format("INSERT INTO `transactiondetails` VALUES (%d, %d, %d", transactionId, itemId, quantity);
+		
+		Connect db = Connect.getInstance();
+		
+		db.execute(query);
+		
 	}
-	public void setTransactionId(Integer transactionId) {
-		this.transactionId = transactionId;
+	
+	public ArrayList<Integer> getAllIdByItem(Integer itemId) {
+		
+		ArrayList<Integer> ids = new ArrayList<>();
+		
+		String query = String.format("SELECT transactionId FROM `transactiondetails` WHERE itemId = %d", itemId);
+		
+		Connect db = Connect.getInstance();
+		
+		ResultSet rs = db.selectData(query);
+		
+		try {
+			while(rs.next()) {
+				Integer transactionId = rs.getInt("transactionId");
+				
+				ids.add(transactionId);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return ids;
+		
 	}
-	public Integer getUserId() {
-		return userId;
+	
+	public void delete(Integer transactionId) {
+		
+		String query = String.format("DELETE from `transactiondetails` WHERE transactionId = %d", transactionId);
+		
+		Connect db = Connect.getInstance();
+		
+		db.execute(query);
+		
 	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
+	
+	public void getTransactionByFan(Integer userId) {
+		
+		String query = String.format("SELECT th.transactionId, th.userId, td.itemId, td.quantity from `transactionheaders` th JOIN `transactiondetails` td ON th.transactionId = td.transactionId WHERE userId = %d", userId);
+		
+		Connect db = Connect.getInstance();
+		
+		db.execute(query);
+		
 	}
-	public Integer getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
+	
+	public ArrayList<TransactionDetail> getTransactionDetail(Integer tId) {
+		
+		ArrayList<TransactionDetail> tds = new ArrayList<>();
+		
+		String query = String.format("SELECT * FROM `transactiondetails` WHERE transactionId = %d", tId);
+		
+		Connect db = Connect.getInstance();
+		
+		ResultSet rs = db.selectData(query);
+		
+		try {
+			while(rs.next()) {
+				Integer transactionId = rs.getInt("transactionId");
+				Integer itemId = rs.getInt("itemId");
+				Integer quantity = rs.getInt("quantity");
+				
+				tds.add(new TransactionDetail(transactionId, itemId, quantity));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return tds;
 	}
 	
 
