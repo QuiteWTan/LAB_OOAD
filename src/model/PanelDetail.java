401f3ba339af73e1a2823f26b8bada35f734e03d
kllerqueen <102903801+kllerqueen@users.noreply.github.com>
2023-11-24 15:40:30 +0700

    added getAttendees function

--- src/model/PanelDetail.java
+++ src/model/PanelDetail.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.sql.SQLException;
+
+import database.Connect;
+
 public class PanelDetail {
 	private Integer panelId;
 	private Integer userId;
@@ -26,6 +30,33 @@ public class PanelDetail {
 		this.userId = userId;
 	}
 	
-	
-	
+	public void addAttendee(Integer panelId, Integer userId) throws SQLException {
+        Connect db = Connect.getInstance();
+
+        String query = String.format("INSERT INTO `PanelDetails` (panelId, userId) VALUES (%d, %d)", panelId, userId);
+
+        db.execute(query);        
+    }
+    
+    public void getAllAttendee(Integer panelId) throws SQLException {
+    	Connect db = Connect.getInstance();
+    	
+    	String query = String.format("SELECT us.* FROM users us JOIN panelDetails pd ON us.userId = pd.userId WHERE pd.panelId = %d", panelId);
+    	
+    	db.execute(query);
+    }
+    
+    public void deletePanel(Integer panelId) throws SQLException {
+        Connect db = Connect.getInstance();
+        
+        String query = String.format("DELETE FROM `PanelDetails` WHERE panelId = %d", panelId);
+        
+        db.execute(query);        
+    }
+    
+    //delete fan attendance
+    
 }
+	
+	
+
